74aed29fe40feaebc2b143cd02612080f724ceb1
edruga <jhe6rv@virginia.edu>
2024-04-21 00:59:15 -0400

    Finished year selection callback

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from dash import Dash, dcc, html, Input, Output, callback
 from urllib.request import urlopen
 import json
 import numpy as np
+import datetime
+import time
 
 # load the CSS stylesheet
 stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -16,7 +18,7 @@ server = app.server
 
 """INITIALIZATION CODE ABOVE, DO NOT EDIT"""
 
-df = pd.read_csv('data_final.csv', dtype={'CountyFIPS':str, 'StateFIPS':str})
+df = pd.read_csv('data_final_2.csv', dtype={'CountyFIPS':str, 'StateFIPS':str})
 
 
 def get_counts_by_county(dataframe):
@@ -39,25 +41,64 @@ graph_div = html.Div([
 state_dropdown_div = html.Div([
     dcc.Dropdown(
         id='state-dropdown',
-        options = df['State'].unique(),
+        options=df['State'].unique(),
         multi=True,
-        value=["VA", "NY", "TX", "CA"],
+        value=df['State'].unique(),
+    )
+])
+
+date_labels_dict = dict()
+for i in range(24):
+    num = i * 4
+    month = str((num % 12) + 1)
+    year_index = num // 12
+    year = str(2016 + year_index)
+    label = f'{year}-{month}'
+    date_labels_dict.update({num: label})
+
+
+date_slider_div = html.Div([
+    dcc.RangeSlider(
+        id='date-slider',
+        min=0,
+        max=95,
+        step=2,
+        value=[0, 95],
+        allowCross=False,
+        marks=date_labels_dict,
+
     )
 ])
 
 app.layout = html.Div([
     graph_div,
-    state_dropdown_div
+    state_dropdown_div,
+    date_slider_div,
 ], className="row")
 
 
 @callback(Output('graph', 'figure'),
-          Input('state-dropdown', 'value'))
-def update_figure(states):
-    print("Callback activated")
-    print(type(states))
-    print(states)
+          Input('state-dropdown', 'value'),
+          Input('date-slider', 'value'))
+def update_figure(states, date_range):
     filtered_df = df[df['State'].isin(states)]
+
+    min_date_index = date_range[0]
+    max_date_index = date_range[1]
+
+    min_month = (min_date_index % 12) + 1
+    min_year_index = min_date_index // 12
+    min_year = 2016 + min_year_index
+
+    max_month = (max_date_index % 12) + 1
+    max_year_index = max_date_index // 12
+    max_year = 2016 + max_year_index
+
+    min_timestamp = int(time.mktime(datetime.datetime(min_year, min_month, 1).timetuple()))
+    max_timestamp = int(time.mktime(datetime.datetime(max_year, max_month, 1).timetuple()))
+
+    filtered_df = filtered_df[filtered_df['timestamp'] >= min_timestamp]
+    filtered_df = filtered_df[filtered_df['timestamp'] <= max_timestamp]
 
     filtered_df_counts = get_counts_by_county(filtered_df)
     filtered_fig = px.choropleth(filtered_df_counts,
@@ -81,7 +122,7 @@ def update_figure(states):
         tickvals=[1, 2, 3, 4, 5],
         ticktext=["10", "100", "1000", "10k", "100k"],
     ))
-    print("Got to end of callback")
+    print("end of callback")
     return filtered_fig
 
 
